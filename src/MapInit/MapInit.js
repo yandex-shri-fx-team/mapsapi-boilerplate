1c63dbfdeab968189b5f498410f3368d7fe35ea2
Vitaliy Andryushkov <vandryushkov@rbc.ru>
2018-04-25 11:59:13 +0300

    пофиксил отмену правила eslint (no-unused-vars) в модуле MapInit

--- src/MapInit/MapInit.js
+++ src/MapInit/MapInit.js
@@ -1,6 +1,6 @@
 const init = (center = [[55.76, 37.64]], zoom = 9, controls = []) => {
     ymaps.ready(() => {
-        // eslint-disable-next-line
+        // eslint-disable-next-line no-unused-vars
         const myMap = new ymaps.Map('map', {
             center: center,
             zoom: zoom,
